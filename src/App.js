5f06ae52c4c1c8dc420f86ba88e2df48858a1ab8
Praveen Bisht <prvnbist@gmail.com>
2018-11-25 13:42:56 +0530

    Migrating to ReactJs

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App"></div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App/>, document.getElementById('root'));
