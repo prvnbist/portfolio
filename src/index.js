185e5b18df2cf14ac18b4c86dc42e12531a83d45
Praveen Bisht <prvnbist@gmail.com>
2018-11-25 14:02:34 +0530

    Setup SCSS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/index.scss';
 
 class App extends React.Component {
     render() {
         return (
-            <div className="App"></div>
+            <div className="App"><h1>Hello</h1></div>
         );
     }
 }
