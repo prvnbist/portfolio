49e70e45af113a70471e8bba64284dbc6884375a
Praveen Bisht <prvnbist@gmail.com>
2019-05-09 23:32:56 +0530

    Added: Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,28 @@
 import React from "react"
+import {Link} from 'gatsby';
 
 const Navbar = () => <nav>
-    <div id="brand">
-      <img src="https://res.cloudinary.com/prvnbist/image/upload/v1557417795/portfolio/brand-light.png" alt="Praveen Bisht"/>
+    <div className="center">
+        <div id="brand">
+            <Link to="/"><img
+                src="https://res.cloudinary.com/prvnbist/image/upload/v1557417795/portfolio/brand-light.png"
+                alt="Praveen Bisht"/></Link>
+        </div>
+        <ul>
+            <li>
+                <Link to="/design" activeClassName="active">Design</Link>
+            </li>
+            <li>
+                <Link to="/code" activeClassName="active">Code</Link>
+            </li>
+            <li>
+                <Link to="/video" activeClassName="active">Video</Link>
+            </li>
+            <li>
+                <Link to="/blog" activeClassName="active">Blog</Link>
+            </li>
+        </ul>
     </div>
-  </nav>
+</nav>
 
 export default Navbar
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/Layout';
+import '../styles/index.scss';
 
 const IndexPage = () => <Layout>
             <h1>Hello.</h1>
