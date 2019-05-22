b4d888b1a4c753ac5a0792078f5f643b8f243632
Praveen Bisht <prvnbist@gmail.com>
2019-05-22 21:01:24 +0530

    Added: Dropdown component

--- src/pages/design.js
+++ src/pages/design.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import Layout from '../components/Layout';
+import Options from '../components/Options';
 
-export default () => <Layout>
-            Design
+export default() => {
+    const optionsList = ['All', 'Web', 'App', 'Animation', 'Icons'];
+    const [designType, setDesignType] = useState('All');
+    let selected = (value) => {
+        setDesignType(value);
+    }
+    return (
+        <Layout>
+            <div className="container">
+                <h4 className="page-heading">Explore Designs</h4>
+                <Options options={optionsList} selected={selected}/>
+            </div>
         </Layout>
-
+    );
+}
