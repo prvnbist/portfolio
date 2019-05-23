468cc1573012f427cb720d1c73c5e5d34c052235
Praveen Bisht <prvnbist@gmail.com>
2019-05-23 17:47:37 +0530

    Added: Hemlet Package

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
         author: 'Praveen Bisht'
     },
     plugins: [
+        `gatsby-plugin-react-helmet`,
         'gatsby-plugin-sass',
         {
             resolve: 'gatsby-source-filesystem',
