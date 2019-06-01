16dc2f1ad8a7139d39203f928af40e0d1f8788eb
Praveen Bisht <prvnbist@gmail.com>
2019-06-01 21:15:55 +0530

    Removed: Comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,30 +34,6 @@ module.exports = {
                           noInlineHighlight: false,
                         },
                       },
-                    // {
-                    //     resolve: `gatsby-remark-vscode`,
-                    //     options: {
-                    //         colorTheme: {
-                    //             defaultTheme: 'Level Up'
-                    //         },
-                    //         wrapperClassName: '', // Additional class put on 'pre' tag
-                    //         injectStyles: true, // Injects (minimal) additional CSS for layout and scrolling
-                    //         extensions: [
-                    //             {
-                    //                 identifier: 'leveluptutorials.theme-levelup',
-                    //                 version: '0.1.2'
-                    //             }
-                    //         ], // Extensions to download from the marketplace to provide more languages and themes
-                    //         languageAliases: {}, // Map of custom/unknown language codes to standard/known language codes
-                    //         replaceColor: x => x, // Function allowing replacement of a theme color with another. Useful for replacing hex colors with CSS variables.
-                    //         getLineClassName: ({ // Function allowing dynamic setting of additional class names on individual lines
-                    //             content, //   - the string content of the line
-                    //             index, //   - the zero-based index of the line within the code fence
-                    //             language, //   - the language specified for the code fence
-                    //             codeFenceOptions //   - any options set on the code fence alongside the language (more on this later)
-                    //         }) => ''
-                    //     }
-                    // }
                 ]
             }
         }, {
