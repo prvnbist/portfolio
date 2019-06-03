deb6a74a9d6d4ee52cb4b38f03669ada54bded6e
Praveen Bisht <prvnbist@gmail.com>
2019-06-04 00:17:35 +0530

    Added: Preloader for Shot image

--- src/templates/shot.js
+++ src/templates/shot.js
@@ -2,8 +2,11 @@ import React from 'react'
 import {Link} from 'gatsby';
 
 import Layout from '../components/Layout';
+import Loading from '../components/Loading';
 
 const Shot = (props) => {
+    const [imgLoaded,
+        setImgLoaded] = React.useState(false);
     const {title, html_url, images, description, tags} = props.pageContext.shot;
     const meta = {
         title: `${title} | Praveen Bisht`,
@@ -11,29 +14,54 @@ const Shot = (props) => {
         keywords: tags,
         imgUrl: {
             google: images.hidpi,
-            facebook:images.hidpi,
+            facebook: images.hidpi,
             twitter: images.hidpi
-        },
+        }
     }
+
     return (
         <Layout meta={meta}>
             <div className="container" id="dribbble-shot">
                 <Link to='/design' className="btn btn-outline-light" id="back">Back</Link>
                 <div id="shot-heading">
                     <h4 className="page-heading">{title}</h4>
-                    <a href={html_url} target="_blank" rel="noopener noreferrer" className="btn btn-primary">View on Dribbble</a>
+                    <a
+                        href={html_url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="btn btn-primary">View on Dribbble</a>
                 </div>
-                <img src={images.hidpi} alt={title} id="shot-image"/>
+                <div
+                    style={{
+                    display: imgLoaded
+                        ? 'none'
+                        : 'block'
+                }}>
+                    <Loading width={100} heigh={'260px'}/>
+                </div>
+                <img
+                    src={images.hidpi}
+                    onLoad={() => setImgLoaded(!imgLoaded)}
+                    style={{
+                    display: imgLoaded
+                        ? 'block'
+                        : 'none'
+                }}
+                    alt={title}
+                    id="shot-image"/>
+
                 <div id="description">
                     <h5 className="caps__heading">Description</h5>
-                    <p dangerouslySetInnerHTML={{ __html: description }}></p>
+                    <p
+                        dangerouslySetInnerHTML={{
+                        __html: description
+                    }}></p>
                 </div>
                 <div id="tags">
                     <h5 className="caps__heading">Tags</h5>
                     <ul>
-                        {
-                            tags.map((tag, index) => <li key={index} className='tag'>{tag}</li>)
-                        }
+                        {tags.map((tag, index) => <li key={index} className='tag'>{tag}</li>)
+}
                     </ul>
                 </div>
 
