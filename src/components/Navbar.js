30350d497864443b1aa1cbe834e4af5bc6993690
Praveen Bisht <prvnbist@gmail.com>
2019-06-05 14:48:20 +0530

    Fixed: Build issue with undefined window object

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,19 +3,26 @@ import {Link} from 'gatsby';
 
 const Navbar = () => {
     let checked = '';
-    if(!localStorage.getItem('theme')){
-        localStorage.setItem('theme',JSON.stringify('light'));
+    if(typeof window !== 'undefined') {
+        if(!localStorage.getItem('theme')){
+            localStorage.setItem('theme',JSON.stringify('light'));
+        }
+        else {
+            checked = JSON.parse(localStorage.getItem('theme')) === 'dark' ? true : '';
+        }
     }
-    else {
-        checked = JSON.parse(localStorage.getItem('theme')) === 'dark' ? true : '';
+    let html = null;
+    if(typeof window !== 'undefined') {
+        html = document.getElementsByTagName('html')[0];
+        html.setAttribute('data-theme', JSON.parse(localStorage.getItem('theme')));
     }
-    const html = document.getElementsByTagName('html')[0];
-    html.setAttribute('data-theme', JSON.parse(localStorage.getItem('theme')));
     let toggleTheme = () => {
-        const selectedTheme = JSON.parse(localStorage.getItem('theme')) === "light" ? "dark" : "light";
-        checked = selectedTheme === 'dark' ? true : '';
-        localStorage['theme'] = JSON.stringify(selectedTheme);
-        html.setAttribute("data-theme", selectedTheme);
+        if(typeof window !== 'undefined') {
+            const selectedTheme = JSON.parse(localStorage.getItem('theme')) === "light" ? "dark" : "light";
+            checked = selectedTheme === 'dark' ? true : '';
+            localStorage['theme'] = JSON.stringify(selectedTheme);
+            html.setAttribute("data-theme", selectedTheme);
+        }
     }
     return (
         <nav>
@@ -39,9 +46,9 @@ const Navbar = () => {
                         <Link to="/blog" activeClassName="active">Blog</Link>
                     </li>
                     <li>
-                        <label class="switch">
+                        <label className="switch">
                             <input type="checkbox" id="themeSelect" defaultChecked={checked} onChange={() => toggleTheme()}/>
-                            <span class="slider round"></span>
+                            <span className="slider round"></span>
                         </label>
                     </li>
                 </ul>
