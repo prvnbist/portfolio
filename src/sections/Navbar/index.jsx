55084764bb6bd05acaae461f46daba9a1f926182
Praveen Bisht <prvnbist@gmail.com>
2020-01-10 23:59:16 +0530

    styles: Navbar

--- src/sections/Navbar/index.jsx
+++ src/sections/Navbar/index.jsx
@@ -1,32 +1,42 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import { StyledNavbar, StyledNavItems, StyledNavItem } from './styles'
+
 const Navbar = () => {
+   let [current, setCurrent] = React.useState('')
    const [links] = React.useState([
       { title: 'Design', url: '/design' },
       { title: 'Code', url: '/code' },
       { title: 'Blog', url: '/blog' }
    ])
    return (
-      <nav>
+      <StyledNavbar>
          <div>
             <div>
                <Link to="/">
                   <img
+                     width="32"
                      src="https://res.cloudinary.com/prvnbist/image/upload/v1559588280/portfolio/logo.png"
                      alt="Praveen Bisht"
                   />
                </Link>
             </div>
-            <ul>
+            <StyledNavItems>
                {links.map(link => (
-                  <li key={link.url}>
-                     <Link to={link.url}>{link.title}</Link>
-                  </li>
+                  <StyledNavItem key={link.url} isActive={link.url === current}>
+                     <Link
+                        to={link.url}
+                        getProps={props =>
+                           props.isCurrent && setCurrent(props.href)
+                        }>
+                        {link.title}
+                     </Link>
+                  </StyledNavItem>
                ))}
-            </ul>
+            </StyledNavItems>
          </div>
-      </nav>
+      </StyledNavbar>
    )
 }
 
---
+++ src/sections/Navbar/styles.js
@@ -0,0 +1,59 @@
+import styled, { css } from 'styled-components'
+
+export const StyledNavbar = styled.nav(
+   () => css`
+      height: 64px;
+      width: 100vw;
+      border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+      > div {
+         width: 980px;
+         display: flex;
+         margin: 0 auto;
+         height: inherit;
+         align-items: center;
+         justify-content: space-between;
+         @media (max-width: 567px) {
+            width: calc(100% - 32px);
+         }
+      }
+   `
+)
+
+export const StyledNavItems = styled.ul(
+   () => css`
+      display: flex;
+      height: inherit;
+   `
+)
+export const StyledNavItem = styled.li(
+   ({ theme: { colors, size }, isActive }) => css`
+      height: inherit;
+      list-style: none;
+      margin-left: ${size.sm};
+      position: relative;
+      ${isActive &&
+         css`
+            &:after {
+               bottom: 0;
+               content: '';
+               width: ${size.sm};
+               height: ${size.xs};
+               position: absolute;
+               left: calc(50% - ${size.xs});
+               background: ${colors.dark['100']};
+               border-top-left-radius: ${size.xs};
+               border-top-right-radius: ${size.xs};
+            }
+         `}
+      a {
+         color: #fff;
+         height: inherit;
+         line-height: 64px;
+         font-weight: 400;
+         display: inline-block;
+         letter-spacing: 0.3px;
+         font-size: ${size.sm};
+         text-decoration: none;
+      }
+   `
+)
