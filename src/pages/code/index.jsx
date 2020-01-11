18421f90ed471d1f4c670cf161473adeccc415d3
Praveen Bisht <prvnbist@gmail.com>
2020-01-11 13:51:41 +0530

    feat(page): Code

--- src/pages/code/index.jsx
+++ src/pages/code/index.jsx
@@ -2,6 +2,76 @@ import React from 'react'
 
 import Layout from '../../sections/Layout'
 
+import { TextButton } from '../../components'
+
+import { PageHeading, Projects, Project } from './styles'
+
 export default () => {
-   return <Layout>Code</Layout>
+   const [projects] = React.useState([
+      {
+         title: 'Transcode',
+         description:
+            'A platform hosting collection of tools for textual transformation.',
+         demo: 'https://transcode.prvnbist.com/',
+         code: 'https://github.com/prvnbist/transcode'
+      },
+      {
+         title: 'Snippify',
+         description:
+            'A snippet management app built with monaco text editor and supports over 30 languages with syntax highlighting.',
+         code: 'https://github.com/prvnbist/snippify'
+      },
+      {
+         title: 'Karya',
+         description:
+            'A simple todo application to manage your tasks and categorize them by using labels.',
+         code: 'https://github.com/prvnbist/karya'
+      },
+      {
+         title: 'Expenses',
+         description:
+            'A platform to empower you in managing expenses packed with analytics for your expenses.',
+         code: 'https://github.com/prvnbist/expense-app-frontend',
+         demo: 'https://expense-app.netlify.com/'
+      },
+      {
+         title: 'Recipe',
+         description: 'A simple recipe for recipe management in one place',
+         code: 'https://codesandbox.io/s/recipe-app-reactredux-z7ngi',
+         demo: 'https://z7ngi.codesandbox.io/'
+      }
+   ])
+   return (
+      <Layout>
+         <PageHeading>Code</PageHeading>
+         <Projects>
+            {projects.map(project => (
+               <Project key={project.url}>
+                  <header>
+                     <h3>{project.title}</h3>
+                  </header>
+                  <main>
+                     <p>{project.description}</p>
+                  </main>
+                  <footer>
+                     {project.code && (
+                        <a href={project.code}>
+                           <TextButton type="solid" typeColor="blue.400">
+                              Code
+                           </TextButton>
+                        </a>
+                     )}
+                     {project.demo && (
+                        <a href={project.demo}>
+                           <TextButton type="outline" typeColor="dark.200">
+                              Demo
+                           </TextButton>
+                        </a>
+                     )}
+                  </footer>
+               </Project>
+            ))}
+         </Projects>
+      </Layout>
+   )
 }
---
+++ src/pages/code/styles.js
@@ -0,0 +1,53 @@
+import styled, { css } from 'styled-components'
+
+export const PageHeading = styled.h1(
+   ({ theme: { size } }) => css`
+      color: #ffffff;
+      font-weight: 400;
+      font-size: ${size.lg};
+      margin: ${size.lg} 0;
+   `
+)
+
+export const Projects = styled.ul(
+   ({ theme: { size } }) => css`
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-auto-rows: minmax(260px, 1fr);
+      grid-gap: ${size.md};
+      @media (max-width: 980px) {
+         grid-template-columns: 1fr;
+      }
+   `
+)
+
+export const Project = styled.li(
+   ({ theme: { size, colors } }) => css`
+      display: flex;
+      flex-direction: column;
+      list-style: none;
+      padding: ${size.md};
+      border-radius: ${size.xs};
+      background: ${colors.dark['300']};
+      header {
+         h3 {
+            font-weight: 500;
+            font-size: ${size.md};
+            margin-bottom: ${size.sm};
+         }
+      }
+      main {
+         height: 100%;
+         p {
+            font-size: 18px;
+            line-height: 27px;
+         }
+      }
+      footer {
+         margin-top: auto;
+         a {
+            margin-right: ${size.sm};
+         }
+      }
+   `
+)
