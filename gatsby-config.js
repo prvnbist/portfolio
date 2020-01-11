db0fa482ba95eacf247cab754c2ea6090e2bdb06
Praveen Bisht <prvnbist@gmail.com>
2020-01-12 00:57:03 +0530

    feat: rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,53 @@ module.exports = {
             head: true
          }
       },
-      `gatsby-plugin-react-helmet`
+      `gatsby-plugin-react-helmet`,
+      {
+         resolve: `gatsby-plugin-feed`,
+         options: {
+            feeds: [
+               {
+                  serialize: ({ query: { site, allMdx } }) => {
+                     return allMdx.edges.map(edge => {
+                        return {
+                           ...edge.node.frontmatter,
+                           description: edge.node.excerpt,
+                           url: `${site.siteMetadata.siteUrl}/blog/${edge.node.fields.slug}`,
+                           custom_elements: [
+                              { 'content:encoded': edge.node.html }
+                           ]
+                        }
+                     })
+                  },
+                  query: `
+                     {
+                        allMdx(
+                           limit: 1000,
+                           sort: {
+                           order: DESC,
+                           fields: [frontmatter___date]
+                           }
+                        ) {
+                           edges {
+                           node {
+                              frontmatter {
+                                 title
+                                 date
+                              }
+                              fields {
+                                 slug
+                              }
+                              excerpt
+                              html
+                           }
+                           }
+                        }
+                     }
+                  `,
+                  output: `rss.xml`
+               }
+            ]
+         }
+      }
    ]
 }
