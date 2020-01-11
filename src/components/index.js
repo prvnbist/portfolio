74c1fe8c1ebfe27aedbc789c09e7b4664a04b267
Praveen Bisht <prvnbist@gmail.com>
2020-01-11 19:29:13 +0530

    feat(component): CodeBlock

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 import { TextButton, IconButton, ComboButton } from './Button'
+import CodeBlock from './CodeBlock'
 
-export { TextButton, IconButton, ComboButton }
+export { TextButton, IconButton, ComboButton, CodeBlock }
