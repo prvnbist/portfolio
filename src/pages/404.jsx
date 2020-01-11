4577f5a81afa125034df63609098dcad46b3fb4e
Praveen Bisht <prvnbist@gmail.com>
2020-01-12 00:56:51 +0530

    feat(page): 404

---
+++ src/pages/404.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Layout from '../sections/Layout'
+
+const NotFound = () => {
+   return (
+      <Layout
+         meta={{
+            title: 'Praveen Bisht | Software Engineer',
+            description:
+               "Hey👋🏼, I’m Praveen, a software engineer based in New Delhi who enjoys building apps from idea to implementation. I've experience with both design & development(front-end & back-end).",
+            keywords:
+               'front end, back end, design, html, pug, css, scss, javascript, nodejs, reactjs, graphql, expressjs, mongoose, mongodb, gatsby, figma, design, user interface, user experience'
+         }}>
+         404!
+      </Layout>
+   )
+}
+
+export default NotFound
