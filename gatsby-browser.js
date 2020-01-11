16d55d8915f90365ea049787b52ff15558a51a15
Praveen Bisht <prvnbist@gmail.com>
2020-01-11 19:31:13 +0530

    build: added prism-react-renderer

--- gatsby-browser.js
+++
@@ -1,1 +0,0 @@
-require('prismjs/plugins/line-numbers/prism-line-numbers.css')
--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,7 @@ module.exports = {
       {
          resolve: `gatsby-plugin-mdx`,
          options: {
-            extensions: [`.mdx`, `.md`],
-            gatsbyRemarkPlugins: [`gatsby-remark-prismjs`]
+            extensions: [`.mdx`, `.md`]
          }
       },
       {
