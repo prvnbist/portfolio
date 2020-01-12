466ab103bc76664896dcbb02ba4bac02099cbfc3
Praveen Bisht <prvnbist@gmail.com>
2020-01-12 20:55:10 +0530

    feat: added markup

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import Layout from '../sections/Layout'
 
@@ -12,7 +13,10 @@ const NotFound = () => {
             keywords:
                'front end, back end, design, html, pug, css, scss, javascript, nodejs, reactjs, graphql, expressjs, mongoose, mongodb, gatsby, figma, design, user interface, user experience'
          }}>
-         404!
+         <h2 style={{ fontWeight: 400, lineHeight: '48px', marginTop: '48px' }}>
+            Looks like you're lost! No worries, I'll help go back safely! Here's{' '}
+            <Link to="/">home</Link>
+         </h2>
       </Layout>
    )
 }
