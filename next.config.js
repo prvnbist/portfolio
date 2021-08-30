a40b9dc10395ee474c03d66f759674bd5d47e785
Praveen Bisht <prvnbist@gmail.com>
2021-08-31 00:23:28 +0530

    build: use webpack 4

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 module.exports = {
+   webpack5: false,
    webpack: (config, { isServer }) => {
       if (!isServer) {
          config.node = { fs: 'empty', module: 'empty' }
