f9e55938a87e835d8ddb8a5058929731828a703d
Praveen Bisht <prvnbist@gmail.com>
2021-08-31 16:44:48 +0530

    feat: website footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import tw from 'twin.macro'
 
 import '../styles/global.css'
 import GlobalStyles from '../styles/global'
@@ -8,6 +9,12 @@ const App = ({ Component, pageProps }) => {
       <>
          <GlobalStyles />
          <Component {...pageProps} />
+         <footer tw="border-t border-gray-800 py-5">
+            <p tw="text-center">
+               ©{new Date().getFullYear()} Made with 💖 using 👩🏽‍💻NextJs, 💾Github
+               & 🌍Netlify.
+            </p>
+         </footer>
       </>
    )
 }
