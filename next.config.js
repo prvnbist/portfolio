a330941bf95bf2473555b26176c801749defe48c
Praveen Bisht <prvnbist@gmail.com>
2021-11-05 21:10:25 +0530

    build: set target to serverless

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 module.exports = {
    webpack5: true,
+   target: 'serverless',
    webpack: config => {
       config.resolve.fallback = {
          ...config.resolve.fallback,
