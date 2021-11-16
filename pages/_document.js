bedba97f90cb0e26839c2a62a4bc4577a304e156
Praveen Bisht <prvnbist@gmail.com>
2021-11-16 21:02:10 +0530

    feat(analytics): umami & google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -2,10 +2,9 @@
 // on load. Without it, the styles are only added once
 // react loads on the frontend
 
-import Document from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
-
-export default class MyDocument extends Document {
+class MyDocument extends Document {
    static async getInitialProps(ctx) {
       const sheet = new ServerStyleSheet()
       const originalRenderPage = ctx.renderPage
@@ -30,4 +29,41 @@ export default class MyDocument extends Document {
          sheet.seal()
       }
    }
+
+   render() {
+      return (
+         <Html>
+            <Head>
+               <script
+                  async
+                  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+               ></script>
+               <script
+                  dangerouslySetInnerHTML={{
+                     __html: `
+                     window.dataLayer = window.dataLayer || []; 
+                     function gtag() {dataLayer.push(arguments)}; 
+                     gtag('js', new Date());
+                     gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                        page_path: window.location.pathname,
+                     });
+                  `,
+                  }}
+               />
+               <script
+                  async
+                  defer
+                  src="https://x-umami.vercel.app/umami.js"
+                  data-website-id="7aced01b-fddd-4700-aa4b-d991b33beed5"
+               ></script>
+            </Head>
+            <body>
+               <Main />
+               <NextScript />
+            </body>
+         </Html>
+      )
+   }
 }
+
+export default MyDocument
