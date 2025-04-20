062c25cac83dc8e69167e8262770fd172db9da9b
Praveen Bisht <prvnbist@gmail.com>
2025-04-20 12:44:10 +0530

    refactor: v2 cleanup

--- src/app/blog/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Articles } from './Articles'
-export { default as Loader } from './Loader'
-export { default as Search } from './Search'
-export { default as Tags } from './Tags'
